bdb5aeeb7a2b44960e88de9199c042e0b0910bfc
Pedro Canassa <pedro_canassa@hotmail.com>
2020-09-19 01:29:10 -0300

    first commit :rocket:

---
+++ src/pages/_document.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document'
+
+class MyDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html lang="pt">
+        <Head>
+          <meta charSet="utf-8" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          ></link>
+        </Head>
+        <body>
+   <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
---
+++ src/pages/index.tsx
@@ -0,0 +1,126 @@
+import React, { memo, useState, useCallback, ChangeEvent, useMemo } from 'react'
+import { saveAs } from 'file-saver'
+import {
+  Grid,
+  Flex,
+  Textarea,
+  RadioGroup,
+  Radio,
+  Box,
+  Button
+} from '@chakra-ui/core'
+
+const Home: React.FC = () => {
+  const [textValue, setTextValue] = useState('')
+
+  const handleInputChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      setTextValue(event.target.value)
+    },
+    []
+  )
+
+  const handleUpperCase = useCallback(() => {
+    setTextValue(previousValue => previousValue.toLocaleUpperCase())
+  }, [])
+
+  const handleLowerCase = useCallback(() => {
+    setTextValue(previousValue => previousValue.toLocaleLowerCase())
+  }, [])
+
+  const handleReverse = useCallback(() => {
+    setTextValue(previousValue => previousValue.split('').reverse().join(''))
+  }, [])
+
+  const handleSentece = useCallback(() => {
+    setTextValue(
+      previousValue =>
+        previousValue.charAt(0).toLocaleUpperCase() + previousValue.slice(1)
+    )
+  }, [])
+
+  const handleCapitalize = useCallback(() => {
+    setTextValue(previousValue =>
+      previousValue
+        .split(' ')
+        .map(word => word.charAt(0).toLocaleUpperCase() + word.slice(1))
+        .join(' ')
+    )
+  }, [])
+
+  const handleDownload = useCallback(() => {
+    const blob = new Blob([textValue], { type: 'text/plain;charset=utf-8' })
+    saveAs(blob, 'Converted.txt')
+  }, [])
+
+  const handlers = useMemo(
+    () => ({
+      upperCase: handleUpperCase,
+      lowerCase: handleLowerCase,
+      reverse: handleReverse,
+      sentece: handleSentece,
+      capitalize: handleCapitalize,
+      download: handleDownload
+    }),
+    [handleUpperCase, handleLowerCase]
+  )
+
+  const handleResizeChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      const { value } = event.target
+      const callFunction = handlers[value]
+      if (callFunction) {
+        callFunction()
+      }
+    },
+    [handlers]
+  )
+
+  return (
+    <Box height="100vh" p={2}>
+      <Grid
+        as="main"
+        templateColumns="1fr 800px 1fr"
+        templateRows="1fr 1fr 1fr"
+        templateAreas="
+        '. . .'
+        '. text .'
+        '. button .'
+        "
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Flex gridArea="text" alignItems="center" flexDirection="column">
+          <RadioGroup
+            variantColor="purple"
+            isInline
+            spacing={5}
+            onChange={handleResizeChange}
+            mb={6}
+          >
+            <Radio value="upperCase">Upper Case</Radio>
+            <Radio value="lowerCase">Lower Case</Radio>
+            <Radio value="reverse">Reverse</Radio>
+            <Radio value="sentence">Sentence</Radio>
+            <Radio value="capitalize">Capitalize</Radio>
+          </RadioGroup>
+          <Textarea
+            value={textValue}
+            onChange={handleInputChange}
+            placeholder="Start typing..."
+            size="lg"
+          />
+        </Flex>
+        <Button
+          gridArea="button"
+          variantColor="purple"
+          onClick={() => handleDownload()}
+        >
+          Download Text
+        </Button>
+      </Grid>
+    </Box>
+  )
+}
+
+export default memo(Home)
